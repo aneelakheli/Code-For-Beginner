d1449365d05b85c370fa1304bce29c58afe6cacf
anilakheli <kanxoakheli22@gmail.com>
2022-08-08 22:23:28 +0545

    adding nine multipication

---
+++ PraticeQuestion/NineMultiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int i =1, multiply;
+	printf("Multiply of Numer 9:\n");
+	while(i<=10){
+		multiply = 9*i;
+		printf("9 * %d = %d\n", i, multiply);
+		i++;
+	}
+	return 0;
+	
+}
